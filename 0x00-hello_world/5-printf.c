400523f6c7ff2ea8333f760400c4c39de507ee3b
Kofi011 <francistsikata19@gmail.com>
2023-03-18 06:39:25 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This prints exactly a text
+ *
+ * Return: 0 when sucessful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
